a7c2597fe735b7b74bb4d3d09ad735fde6db6a0b
Brett Wines <bwines@quip.com>
2020-12-18 17:04:26 -0500

    Bump version
    Followup to c9c1d7c

--- symbol-navigation-hydra.el
+++ symbol-navigation-hydra.el
@@ -6,7 +6,7 @@
 ;; Keywords: highlight face match convenience hydra symbol
 ;; Package-Requires: ((auto-highlight-symbol "1.53") (hydra "0.15.0") (emacs "24.4") (multiple-cursors "1.4.0"))
 ;; URL: https://github.com/bgwines/symbol-navigation-hydra
-;; Version: 0.0.3
+;; Version: 0.0.4
 
 ;; This file is NOT part of GNU Emacs.
 
